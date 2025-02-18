36d56e71298778ed4c5554b534703930cee1ef66
Gyan Vardhan <115806215+TheNoob51@users.noreply.github.com>
2025-02-18 18:23:51 +0530

    rectification

--- server.js
+++ server.js
@@ -9,16 +9,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const DEFAULT_PORT = 5000;
+const PORT = process.env.PORT || DEFAULT_PORT;
+
+const db = mongoose.connection;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
 db.once("open", () => console.log("Connected to MongoDB"));
+db.on("error", (err) => console.error("MongoDB connection error:", err));
+});
 
 // Routes
 app.use("/hospitals", require("./routes/hospitalRoutes"));
