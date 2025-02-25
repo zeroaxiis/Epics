f2c654d1a75a6f7ae06d4ba5dc28c1ac1bb24859
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2025-02-25 09:53:17 +0530

    Added appointment model

--- src/models/appointment.models.js
+++ src/models/appointment.models.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const AppointmentSchema = new mongoose.Schema(
     {
         patientName:{
-            type: String,
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "User",
             required: true,
             trim: true
         },
